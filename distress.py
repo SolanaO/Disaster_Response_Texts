3fdbf28eada5167fb8852b84186b74d70b513190
SolanaO <silviaEonofrei@gmail.com>
2021-11-06 15:10:05 -0400

    Uncomment the run command to run the app locally.

--- distress.py
+++ distress.py
@@ -2,4 +2,4 @@ from essentials import tokenize
 from distressapp import app
 
 # to run the app locally, for online deployment uncomment this
-#app.run(host='0.0.0.0', port=3001, debug=True)
+app.run(host='0.0.0.0', port=3001, debug=True)
